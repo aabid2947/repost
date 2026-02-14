31a0fd6d32cc33286abd512403acdf31a011a56b
aabid2947 <aabidhussainpas@gmail.com>
2026-02-14 22:42:14 +0530

    added adsense script to run ads

--- app/layout.tsx
+++ app/layout.tsx
@@ -85,6 +85,12 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
+        <head>
+          <script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8418828678583686"
+            crossOrigin="anonymous" />
+        </head>
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
